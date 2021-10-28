3dc652c4c12de941224651510f0efd7c83be3244
rlyabc <416297931@qq.com>
2021-10-29 00:03:03 +0800

    ad;

--- app/helpers.php
+++ app/helpers.php
@@ -13,12 +13,20 @@ function get_db_config()
             'password'  => $url["pass"],
         ];
     } else {
+        $url = parse_url(getenv("DATABASE_URL"));
+//        return $db_config = [
+//            'connection' => env('DB_CONNECTION', 'mysql'),
+//            'host' => env('DB_HOST', 'localhost'),
+//            'database'  => env('DB_DATABASE', 'forge'),
+//            'username'  => env('DB_USERNAME', 'forge'),
+//            'password'  => env('DB_PASSWORD', ''),
+//        ];
         return $db_config = [
-            'connection' => env('DB_CONNECTION', 'mysql'),
-            'host' => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
         ];
     }
 }
