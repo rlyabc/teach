6f9d6769a7256f8587a6aae51ad371d49f19d7b4
rlyabc <416297931@qq.com>
2021-10-29 00:14:28 +0800

    ad;

--- app/helpers.php
+++ app/helpers.php
@@ -5,13 +5,14 @@ function get_db_config()
     if (getenv('IS_IN_HEROKU')) {
         $url = parse_url(getenv("DATABASE_URL"));
 
-        return $db_config = [
+        $db_config = [
             'connection' => 'pgsql',
             'host' => $url["host"],
             'database'  => substr($url["path"], 1),
             'username'  => $url["user"],
             'password'  => $url["pass"],
         ];
+        dd($db_config);
     } else {
         return $db_config = [
             'connection' => env('DB_CONNECTION', 'mysql'),
