2bf8d0687c0f93c291148bee68b68a97f27e5949
rlyabc <416297931@qq.com>
2021-10-29 00:30:33 +0800

    ad;

--- database/migrations/2014_10_12_000000_create_message_notify_table.php
+++ database/migrations/2014_10_12_000000_create_message_notify_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMessagNotifyTable extends Migration
+class CreateMessageNotifyTable extends Migration
 {
     /**
      * Run the migrations.
