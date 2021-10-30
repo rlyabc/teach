f1cfa85a4d9c8678f42dfee2a775dbb21a0f4b5a
rlyabc <416297931@qq.com>
2021-10-30 18:29:27 +0800

    Updated database configuration

--- ws_message_notify.php
+++ ws_message_notify.php
@@ -6,7 +6,7 @@ require_once './Workerman/Autoloader.php';
 require_once './Workerman/vendor/autoload.php';
 
 // 初始化一个worker容器，监听2000端口
-$worker = new Worker('websocket://0.0.0.0:433');//
+$worker = new Worker('websocket://0.0.0.0:3000');//
 /*
  * 注意这里进程数必须设置为1，否则会报端口占用错误
 
