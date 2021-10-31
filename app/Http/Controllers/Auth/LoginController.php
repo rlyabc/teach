ac27fc69845ea4c4f32e43c281d0d1e1c42ab234
rlyabc <416297931@qq.com>
2021-11-01 04:09:34 +0800

    add

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -142,6 +142,7 @@ class LoginController extends Controller
                 'grant_type' => 'authorization_code',
                 'client_id' => config('services.line_api.appid'),
                 'client_secret' => config('services.line_api.secret'),
+                'redirect_uri' => config('services.line_api.callback'),
                 'code' => $request->input('line_user_id'),
                 'scope' => '*',
                 'guard' => 'line_api'
