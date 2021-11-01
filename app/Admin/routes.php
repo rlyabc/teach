bb00480694c23faf430f346680f60aedebca77c1
rlyabc <416297931@qq.com>
2021-11-01 23:40:02 +0800

    add

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -9,7 +9,7 @@ Route::group([
     'namespace'     => config('admin.route.namespace'),
     'middleware'    => config('admin.route.middleware'),
 ], function (Router $router) {
-//    $router->get('/', 'HomeController@index');
+    $router->get('/', 'HomeController@index');
 //    $router->get('users', 'UsersController@index');
 //    $router->get('users/{id}/edit','UsersController@edit');
 //    $router->patch('users/{id}','UsersController@update');
