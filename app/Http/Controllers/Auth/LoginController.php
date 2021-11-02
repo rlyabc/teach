ce96fb5fd6b071b6cfac68019869cf84fe812330
rlyabc <416297931@qq.com>
2021-11-02 08:57:03 +0800

    add

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -49,7 +49,7 @@ class LoginController extends Controller
 
     public function login(Request $request)
     {
-//        try {
+        try {
            $type=$request->input('type');
             $lineUserId=$request->input('line_user_id');
            if($type=='teacher'){
@@ -81,12 +81,12 @@ class LoginController extends Controller
                );
            }
 
-//        } catch (\Exception $e) {
-//            return array(
-//                'code'=>1001,
-//                'msg' =>'账号验证失败1'
-//            );
-//        }
+        } catch (\Exception $e) {
+            return array(
+                'code'=>1001,
+                'msg' =>'账号验证失败1'
+            );
+        }
 
         if ($request->getStatusCode() == 401) {
             return array(
