41eaa43f386561605474d965451a0b95feb2e676
rlyabc <416297931@qq.com>
2021-11-07 20:49:15 +0800

    add

--- database/migrations/2014_10_12_000000_create_line_message_users_table.php
+++ database/migrations/2014_10_12_000000_create_line_message_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class LineMessageUsersTable extends Migration
+class CreateLineMessageUsersTable extends Migration
 {
     /**
      * Run the migrations.
